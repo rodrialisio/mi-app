2a54fe4391110cf1ee676df89343825c03ab94a8
Rodrigo Alisio <rodrigotalisio@gmail.com>
2021-10-22 20:44:38 -0300

    primer desafio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
-function App() {
+export default function App() {
+  class Usuario {
+    constructor(nombre, apellido, libros, mascotas) {
+        this.nombre= nombre
+        this.apellido= apellido
+        this.mascotas= mascotas
+        this.libros= libros
+    }
+
+    newUser() {
+      this.nombre=prompt("nombre del usuario:")
+      if (!this.nombre) {this.nombre="?"}
+      this.apellido=prompt("apellido del usuario:")
+      if (!this.apellido) {this.apellido="?"}
+    }
+
+    getFullName() {
+        console.log(`${this.nombre} ${this.apellido}`)
+        return `${this.nombre} ${this.apellido}`
+    }
+
+    addMascota() {
+        let mascotaNueva= prompt("nombre:")
+        if (mascotaNueva) {this.mascotas.push(mascotaNueva)} // no es agregada si no se ingresa un nombre
+    }
+
+    countMascotas() {
+        console.log(this.mascotas.length)
+        return this.mascotas.length
+    }
+
+    addBook() {
+        let libroNuevo= {titulo: prompt("titulo:"), autor: prompt("autor:")} 
+        if (libroNuevo.titulo && libroNuevo.autor) {this.libros.push(libroNuevo)} // no es agregado si no ingresan ambos datos
+    }
+
+    getBookNames() {
+        console.log(this.libros.map(libro => libro.titulo))
+        return this.libros.map(libro => libro.titulo)
+    }
+  }
+ 
+  const usuario= new Usuario("?","?",[],[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="botones">
+        <button onClick={()=> usuario.newUser()}>crear usuario</button>
+        <button onClick={()=> usuario.getFullName()}>nombre completo</button>
+        <button onClick={()=> usuario.addMascota()}>agregar mascota</button>
+        <button onClick={()=> usuario.countMascotas()}>contar mascotas</button>
+        <button onClick={()=> usuario.addBook()}>agregar libro</button>
+        <button onClick={()=> usuario.getBookNames()}>nombres de libros</button>
+      </div>
+  )
 }
-
-export default App;
---
+++ src/readme.txt
@@ -0,0 +1,3 @@
+Desafío clase 2:
+
+Los métdodos que devuelven datos lo hacen como valores (return) y por consola para visualizarlos
